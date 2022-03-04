10f331dee5059336412ac83dc87d8d6d6dbe5d2e
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2022-03-04 17:50:55 +0500

    TelegramBot

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+using AkissBot.TelegramBot;
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<TelegramBotClient>(b => new TelegramBotClient(builder.Configuration["Telegram:Token"]));
+builder.Services.AddHostedService<Bot>();
+builder.Services.AddTransient<BotHandlers>();
 
 var app = builder.Build();
 
